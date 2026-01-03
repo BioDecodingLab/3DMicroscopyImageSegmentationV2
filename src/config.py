dfbf6fe2863133642d5a40c252ffee0bbfb1839c
WestSopho <153777405+WestSopho@users.noreply.github.com>
2026-01-02 21:58:01 -0500

    remove global path definitions since DatasetPaths will be in charge of this

--- src/config.py
+++ src/config.py
@@ -1,32 +1,8 @@
-from pathlib import Path
-
-from dotenv import load_dotenv
-from loguru import logger
-
-# Load environment variables from .env file if it exists
-load_dotenv()
-
-# Paths
-PROJ_ROOT = Path(__file__).resolve().parents[1]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
-
+# File format configuration
 ALLOWED_EXTENSIONS = (".tiff", ".tif")
 OUTPUT_EXTENSION = ".tif"
 
-DATA_DIR = PROJ_ROOT / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
-INTERIM_DATA_DIR = DATA_DIR / "interim"
-PROCESSED_DATA_DIR = DATA_DIR / "processed"
-EXTERNAL_DATA_DIR = DATA_DIR / "external"
-
-MODELS_DIR = PROJ_ROOT / "models"
-LOGS_DIR = PROJ_ROOT / "logs"
-
-REPORTS_DIR = PROJ_ROOT / "reports"
-FIGURES_DIR = REPORTS_DIR / "figures"
-
-# VARIABLES
-
+# Patch configuration
 PATCH_SIZE = (64, 64, 64)
 PATCH_STEP = 64
 
